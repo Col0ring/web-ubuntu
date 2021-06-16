fc6e4f242bfa11d075a9e83e737ad335569b5fc6
Col0ring <1561999073@qq.com>
2021-06-16 13:40:24 +0800

    feat:update page-loading

--- src/components/page-loading.tsx
+++ src/components/page-loading.tsx
@@ -1,23 +1,32 @@
 import React from 'react'
-import LoadingSpinner from '@/assets/themes/Yaru/status/process-working-symbolic.svg'
 import Dot from './dot'
 
 interface PageLoadingProps {
   tips?: string
+  loading?: boolean
 }
 
-const PageLoading: React.FC<PageLoadingProps> = ({ tips = 'loading' }) => {
+const PageLoading: React.FC<PageLoadingProps> = ({
+  tips = 'loading',
+  loading,
+  children
+}) => {
   return (
-    <div className="fixed top-0 left-0 w-screen h-screen bg-black flex flex-col items-center justify-center">
-      <img
-        className="animate-spin w-20"
-        src={LoadingSpinner}
-        alt="Loading Spinner"
-      />
-      <p className="text-white">
-        <Dot>{tips}</Dot>
-      </p>
-    </div>
+    <>
+      {loading && (
+        <div className="z-50 fixed top-0 left-0 w-screen h-screen bg-black flex flex-col items-center justify-center">
+          <img
+            className="animate-spin w-20"
+            src="/themes/Yaru/status/process-working-symbolic.svg"
+            alt="Loading Spinner"
+          />
+          <p className="text-white">
+            <Dot>{tips}</Dot>
+          </p>
+        </div>
+      )}
+      {children}
+    </>
   )
 }
 
