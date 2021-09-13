140f1f9d7849c647ea2069a5d64b1ebf14dc5472
Col0ring <47329987+Col0ring@users.noreply.github.com>
2021-09-13 11:34:51 +0000

    Deploying to gh-pages from @ Col0ring/web-ubuntu@ead3b258a98490c66af3d9a34540392983d69210 🚀

---
+++ assets/vscode.8108eb09.js
@@ -0,0 +1,1 @@
+import{R as e}from"./vendor.b2c3c829.js";const s=()=>e.createElement("iframe",{src:"https://github1s.com/Col0ring/web-ubuntu/blob/main/src/pages/desktop/index.tsx",frameBorder:"0",title:"VsCode",className:"h-full w-full bg-ub-cool-grey"});export{s as default};
