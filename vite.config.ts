731b0e4c3321ac2b84a8226a752424f066dd0d07
Col0ring <1561999073@qq.com>
2021-09-17 15:50:36 +0800

    chore: update deps

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import reactRefresh from '@vitejs/plugin-react-refresh'
 import eslintPlugin from 'vite-plugin-eslint'
 import viteStylelint from '@amatlash/vite-plugin-stylelint'
 import viteMockPlugin from '@col0ring/vite-plugin-mock'
-import vitePluginCopy from './build/vite-plugin-copy'
+import vitePluginCopy from '@col0ring/vite-plugin-copy'
 import { base } from './src/config'
 
 function resolve(relativePath: string) {
@@ -16,6 +16,12 @@ export default defineConfig({
   base,
   plugins: [
     reactRefresh(),
+    vitePluginCopy([
+      {
+        src: resolve('./dist/index.html'),
+        target: resolve('./dist/404.html'),
+      },
+    ]),
     viteMockPlugin({
       dir: resolve('mock'),
       exclude: /utils/,
@@ -27,9 +33,6 @@ export default defineConfig({
     viteStylelint({
       include: /.*\.(less|css)/,
       exclude: ['node_modules', 'dist', 'dist-ssr'],
-    }),
-    vitePluginCopy({
-      [resolve('./dist/index.html')]: resolve('./dist/404.html'),
     }),
   ],
   css: {
